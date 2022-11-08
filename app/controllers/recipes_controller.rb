a60ed4334c0bd73980d11055b692c7c9b05ab84c
Kinyua <alfkinyua@gmail.com>
2022-11-08 18:38:51 +0300

    Add user references

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,39 @@
+class RecipesController < ApplicationController
+
+  # GET /recipes or /recipes.json
+  def index
+    @user = User.find(current_user.id) 
+    @recipes = Recipe.where(user_id: @user.id) 
+  end
+
+  # GET /recipes/1 or /recipes/1.json
+  def show
+    @recipe=Recipe.find(params[:id]) 
+  end
+
+  # GET /recipes/new
+  def new
+    @recipe = Recipe.new
+  end
+
+
+
+
+  # DELETE /recipes/1 or /recipes/1.json
+  def destroy
+    @recipe=Recipe.find(params[:id]) 
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Only allow a list of trusted parameters through.
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id) 
+    end
+end
