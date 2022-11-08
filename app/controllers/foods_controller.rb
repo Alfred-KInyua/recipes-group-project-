ba605ebb413469dd35c7b8ec5a749d4869511bfc
tugsem <saranlitugsem@gmail.com>
2022-11-08 17:24:17 +0300

    Implement necessary crud methods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,26 +1,16 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[ show edit update destroy ]
-
   def index
-    @foods = Food.all
+    @foods = current_user.foods
   end
 
-  def show
-  end
-
-  def new
-    @food = Food.new
-  end
-
-  def edit
-  end
+  def new; end
 
   def create
-    @food = Food.new(food_params)
+    @food = current_user.foods.new(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -30,20 +20,18 @@ class FoodsController < ApplicationController
   end
 
   def destroy
+    @food = Food.find(params[:id])
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    def set_food
-      @food = Food.find(params[:id])
-    end
 
-    def food_params
-      params.fetch(:food, {})
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
