2b35fed67295e683df7227addd3d740c3ef49f67
tugsem <saranlitugsem@gmail.com>
2022-11-08 17:22:32 +0300

    Add home root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods, except: :update
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
