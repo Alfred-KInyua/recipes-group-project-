78ad0f8cbd6e19f1a380c72fe7f86b047427ee3c
Kinyua <alfkinyua@gmail.com>
2022-11-09 13:06:11 +0300

    Add recipe model validation rules

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+    belongs_to :user, foreign_key: :user_id
+    validates :name, presence: true
+    validates :preparation_time, presence: true
+    validates :cooking_time, presence: true                                                     
+    validates :quantity, presence: true
+      
 end
